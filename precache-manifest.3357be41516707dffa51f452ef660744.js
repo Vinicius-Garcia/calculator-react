c8eefc98efd6db8d97ddee2e92f28d2e6a57f085
Vinicius <viniciusgarcia1300@gmail.com>
2020-10-17 12:06:20 -0300

    Updates

---
+++ precache-manifest.3357be41516707dffa51f452ef660744.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99867f5e15d3a52148d4625940c83937",
+    "url": "/calculator-react/index.html"
+  },
+  {
+    "revision": "eaff412dc9ae893d546a",
+    "url": "/calculator-react/static/css/main.1a37037a.chunk.css"
+  },
+  {
+    "revision": "1f0d6b114442ea665169",
+    "url": "/calculator-react/static/js/2.fadc0b3f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/calculator-react/static/js/2.fadc0b3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaff412dc9ae893d546a",
+    "url": "/calculator-react/static/js/main.920ac8d5.chunk.js"
+  },
+  {
+    "revision": "6d62c3c1411031eb8e1d",
+    "url": "/calculator-react/static/js/runtime-main.fef25df0.js"
+  }
+]);
